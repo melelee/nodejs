039eadcf744b960a0f70847e9a35e3ca0c668f23
menglili <melelee@qq.com>
2024-01-25 22:54:17 +0800

    null,undefined,boolean.

---
+++ html/js/boolean.js
@@ -0,0 +1,13 @@
+let b = true;
+
+
+console.log(b);
+
+console.log(typeof b);
+
+
+console.log(b + 1);
+
+
+b = false;
+console.log(b + 1);
---
+++ html/js/null.js
@@ -0,0 +1,11 @@
+var a;
+
+console.log(a);
+
+
+a = undefined;
+console.log(a);
+
+
+a = null;
+console.log(a);

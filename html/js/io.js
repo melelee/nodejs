9f1f3649bf28db4faf626e02e17cadbe3259c705
menglili <melelee@qq.com>
2024-01-31 22:29:57 +0800

    null,undefined,boolean.

---
+++ html/js/io.js
@@ -0,0 +1,19 @@
+document.write('document write')
+
+document.write('<h1>标题</h1>')
+
+console.log('123123')
+
+const string = window.prompt(`请输入`);
+
+document.write(string)
+
+console.log(string)
+
+
+console.log(name)
+var name = 11111
+
+
+console.log(age)
+let age = 10

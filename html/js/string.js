b95f9fc3f7b3b370ca5c6d9a4c33abc48312ddfa
menglili <melelee@qq.com>
2024-03-03 23:04:26 +0800

    模板字符串

--- html/js/string.js
+++ html/js/string.js
@@ -15,3 +15,20 @@ window.alert('刚满' + window.prompt('你多大了？') + '岁');
 
 
 console.log(`这个是模板${message}字符串`)
+
+
+console.log(1-'a')
+
+console.log(Number('123'))
+console.log(window.Number('1233'))
+
+let number = parseInt(`123.3`);
+console.log(number)
+
+
+
+let number1 = parseFloat('123.4');
+console.log(number1 )
+
+
+

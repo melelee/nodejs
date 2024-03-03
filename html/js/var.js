ae628f0d15a8f67f7950100a4575941bc43aaa49
menglili <melelee@qq.com>
2024-03-03 23:52:57 +0800

    模板字符串

--- html/js/var.js
+++ html/js/var.js
@@ -4,3 +4,14 @@ console.error(xiaohong);
 
 console.log(xiaoming);
 console.log(xiaoliang)
+
+
+let name = ['zhangsan', 'lisi']
+name = ['123']
+
+
+document.querySelector('div')
+document.querySelector('.class')
+document.querySelector('#id')
+document.querySelector('ul li:first-child')
+document.querySelectorAll('').for
